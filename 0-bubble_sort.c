6f6a134243fd66971ba60a5929328eff2de2f1fb
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-22 09:51:35 -0700

    (feat) -> create a bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of int by bubbling up the
+ * highest value up the array
+ * @array: the array to sort
+ * @size: the size of the array
+ * Description: Bubble sort algorithm has a best case scenerio
+ * of O(n) and a worst case scenerio of O(n^2)
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, swap = 1, n = size;
+
+	while (swap)
+	{
+		swap = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				array[i] ^= array[i + 1];
+				array[i + 1] ^= array[i];
+				array[i] ^= array[i + 1];
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+		n--;
+	}
+}
