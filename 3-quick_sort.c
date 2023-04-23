7da7c329ddeec3261bd74af957f352f8ce29966c
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-23 16:58:20 -0700

    (feat) -> implement a quick sort algorithm using Lomuto's techiques

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers using lomuto
+ * quick sort algorthm
+ * @array: the array to sort
+ * @size: the size of the array
+ *nReturn: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size <= 0)
+		return;
+
+	lomuto_quicksort(array, 0, (int) size - 1, size);
+}
+
+/**
+ * lomuto_quicksort - reculsively sorts an array of integers using
+ * lomtumo techniques while partitioning the array on each call
+ * @array: the array to sort
+ * @start: the start of the partitioned array
+ * @end: the end of the partitioned array
+ * @size: the initial size of the array
+ * Return: void
+ */
+void lomuto_quicksort(int *array, int start, int end, size_t size)
+{
+	int idx;
+
+	if (start >= end || start < 0)
+		return;
+
+	idx = partition(array, start, end, size);
+	lomuto_quicksort(array, start, idx - 1, size);
+	lomuto_quicksort(array, idx + 1, end, size);
+}
+
+
+/**
+ * partition - partions and sorts an array using the last index as pivot
+ * @array: the array to sort
+ * @start: the start of the partitioned array
+ * @end: the end of the partitioned array
+ * @size: the initial size of the array
+ * Return: end of the next partion to sort
+ */
+int partition(int *array, int start, int end, size_t size)
+{
+	int pivot, pivot_idx, i, tmp;
+
+	pivot = array[end];
+	pivot_idx = start;
+
+	for (i = start; i < end; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			tmp = array[pivot_idx];
+			array[pivot_idx] = array[i];
+			array[i] = tmp;
+			++pivot_idx;
+		}
+	}
+	tmp = array[pivot_idx];
+	array[pivot_idx] = array[end];
+	array[end] = tmp;
+	print_array(array, size);
+	return (pivot_idx);
+}
--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int start, int end, size_t size);
+void lomuto_quicksort(int *array, int start, int end, size_t size);
 
 #endif /*SORT_H*/
