9f4609ef1e27dcdc3296afb97511ad88172a2463
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-04-30 08:53:10 -0700

    (fix) -> check for NULL head

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *behind, *forward, *cache;
 
-	if (!*list || !(*list)->next)
+	if (!list || !*list || !(*list)->next)
 		return;
 
 	forward = (*list)->next;
